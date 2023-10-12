f621eeb295f35d3e8662016ecff9cf033a403dce
Tamires Patrocinio <tamirespatrocinio.ti@gmail.com>
2023-10-12 17:22:48 -0300

    feat: Lombok Library and Database Integration

--- src/main/java/br/com/tamires/todolist/user/UserModel.java
+++ src/main/java/br/com/tamires/todolist/user/UserModel.java
@@ -1,33 +1,34 @@
 package br.com.tamires.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+//@Setter
+//@Getter
+//@Data p/ ambos
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(unique = true)
   private String username;
   private String name;
   private String password;
 
-  // inserir o valor
-  public void setUsername(String username) {
-    this.username = username;
-  }
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
-  // buscar o valor
-  public String getUsername() {
-    return username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getPassword() {
-    return password;
-  }
 }
